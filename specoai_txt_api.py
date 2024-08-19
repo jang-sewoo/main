4d3fd639df9cf9a6972003edacf8a03595f2f343
jang-sewoo <jangsewoo@gmail.com>
2024-08-19 12:51:37 +0900

    Add files via upload

---
+++ specoai_txt_api.py
@@ -0,0 +1,169 @@
+import streamlit as st
+import tiktoken
+from loguru import logger
+import os
+import gdown
+import pickle
+
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import Docx2txtLoader
+from langchain.document_loaders import UnstructuredPowerPointLoader
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+
+from langchain.memory import ConversationBufferMemory
+from langchain.vectorstores import FAISS
+
+# from streamlit_chat import message
+from langchain.callbacks import get_openai_callback
+from langchain.memory import StreamlitChatMessageHistory
+
+CACHE_FILE = "cached_vectorstore.pkl"
+
+def main():
+    st.set_page_config(
+        page_title="DirChat",
+        page_icon=":books:"
+    )
+
+    st.title("_Private Data :red[QA Chat]_ :books:")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    if "processComplete" not in st.session_state:
+        st.session_state.processComplete = None
+
+    # Get OpenAI API key from environment variable
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+
+    if not openai_api_key:
+        st.info("Please set the OpenAI API key as an environment variable.")
+        st.stop()
+
+    # Check if the vectorstore is already cached
+    if os.path.exists(CACHE_FILE) and not st.session_state.processComplete:
+        with open(CACHE_FILE, "rb") as f:
+            vetorestore = pickle.load(f)
+        st.session_state.conversation = get_conversation_chain(vetorestore, openai_api_key)
+        st.session_state.processComplete = True
+        logger.info("Loaded vectorstore from cache.")
+    elif not st.session_state.processComplete:
+        file_urls = [
+            "https://drive.google.com/uc?id=1GAiYG3Lpl6DYrXzR9poz0LfOBY9gEhUT"
+            ]
+
+
+        files_text = get_text(file_urls)
+        text_chunks = get_text_chunks(files_text)
+        vetorestore = get_vectorstore(text_chunks)
+
+        # Cache the vectorstore for future use
+        with open(CACHE_FILE, "wb") as f:
+            pickle.dump(vetorestore, f)
+
+        st.session_state.conversation = get_conversation_chain(vetorestore, openai_api_key)
+        st.session_state.processComplete = True
+        logger.info("Processed and cached the vectorstore.")
+
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = [{"role": "assistant", 
+                                        "content": "안녕하세요! 주어진 문서에 대해 궁금하신 것이 있으면 언제든 물어봐주세요!"}]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    history = StreamlitChatMessageHistory(key="chat_messages")
+
+    # Chat logic
+    if query := st.chat_input("질문을 입력해주세요."):
+        st.session_state.messages.append({"role": "user", "content": query})
+
+        with st.chat_message("user"):
+            st.markdown(query)
+
+        with st.chat_message("assistant"):
+            chain = st.session_state.conversation
+
+            with st.spinner("Thinking..."):
+                result = chain({"question": query})
+                with get_openai_callback() as cb:
+                    st.session_state.chat_history = result['chat_history']
+                response = result['answer']
+                source_documents = result['source_documents']
+
+                st.markdown(response)
+                with st.expander("참고 문서 확인"):
+                    st.markdown(source_documents[0].metadata['source'], help = source_documents[0].page_content)
+                    st.markdown(source_documents[1].metadata['source'], help = source_documents[1].page_content)
+                    st.markdown(source_documents[2].metadata['source'], help = source_documents[2].page_content)
+
+        # Add assistant message to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+def tiktoken_len(text):
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    tokens = tokenizer.encode(text)
+    return len(tokens)
+
+def get_text(urls):
+    doc_list = []
+    
+    for url in urls:
+        file_name = gdown.download(url, quiet=False)
+        logger.info(f"Downloaded {file_name}")
+        if file_name.endswith('.pdf'):
+            loader = PyPDFLoader(file_name)
+            documents = loader.load_and_split()
+        elif file_name.endswith('.docx'):
+            loader = Docx2txtLoader(file_name)
+            documents = loader.load_and_split()
+        elif file_name.endswith('.pptx'):
+            loader = UnstructuredPowerPointLoader(file_name)
+            documents = loader.load_and_split()
+
+        doc_list.extend(documents)
+    return doc_list
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=900,
+        chunk_overlap=100,
+        length_function=tiktoken_len
+    )
+    chunks = text_splitter.split_documents(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = HuggingFaceEmbeddings(
+                                        model_name="jhgan/ko-sroberta-multitask",
+                                        model_kwargs={'device': 'cpu'},
+                                        encode_kwargs={'normalize_embeddings': True}
+                                        )  
+    vectordb = FAISS.from_documents(text_chunks, embeddings)
+    return vectordb
+
+def get_conversation_chain(vetorestore, openai_api_key):
+    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name='gpt-3.5-turbo', temperature=0)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+            llm=llm, 
+            chain_type="stuff", 
+            retriever=vetorestore.as_retriever(search_type='mmr', vervose=True), 
+            memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
+            get_chat_history=lambda h: h,
+            return_source_documents=True,
+            verbose=True
+        )
+
+    return conversation_chain
+
+if __name__ == '__main__':
+    main()
