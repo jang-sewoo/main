ece386ef77920b7d8c076ba91c35743e42c042d2
jang-sewoo <jangsewoo@gmail.com>
2024-08-19 15:03:21 +0900

    Update specoai_assistant.py

--- specoai_assistant.py
+++ specoai_assistant.py
@@ -1,24 +1,34 @@
+import requests
 import streamlit as st
-import openai
 import os
 
+# OpenAI API 키
+openai_api_key = os.getenv("OPENAI_API_KEY")
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {openai_api_key}",
+    "OpenAI-Beta": "assistants=v2"
+}
+
+# 비서를 통한 질문 응답 함수
+def ask_question(assistant_id, query):
+    url = f"https://api.openai.com/v1/assistants/{assistant_id}/messages"
+    data = {
+        "role": "user",
+        "content": query
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()
+
 def main():
-    st.set_page_config(
-        page_title="DirChat",
-        page_icon=":books:"
-    )
+    st.set_page_config(page_title="Speco API", page_icon=":books:")
 
-    st.title("_SPECO Data :red[QA Chat]_ :books:")
+    st.title("Speco API Assistant")
 
-    if "messages" not in st.session_state:
-        st.session_state.messages = [{"role": "assistant", 
-                                      "content": "안녕하세요! 궁금하신 것이 있으면 언제든 물어봐주세요!"}]
-
-    openai_api_key = os.getenv("OPENAI_API_KEY")
-
-    if openai_api_key is None:
-        st.info("OpenAI API 키를 환경 변수로 설정해주세요.")
-        st.stop()
+    if "assistant_id" not in st.session_state:
+        # 비서 ID를 세션 상태에 저장
+        st.session_state.assistant_id = "asst_t3toIekxpUOO5BYzVniTy0Uc"
+        st.session_state.messages = [{"role": "assistant", "content": "안녕하세요! 무엇을 도와드릴까요?"}]
 
     # 세션 상태에서 대화 기록을 불러오기
     for message in st.session_state.messages:
@@ -34,22 +44,11 @@ def main():
 
         with st.chat_message("assistant"):
             with st.spinner("생각 중..."):
-                response = get_assistant_response(query, openai_api_key)
-                st.markdown(response)
+                response = ask_question(st.session_state.assistant_id, query)
+                assistant_response = response["choices"][0]["message"]["content"]
+                st.markdown(assistant_response)
 
-        # 비서 응답을 대화 기록에 추가
-        st.session_state.messages.append({"role": "assistant", "content": response})
-
-def get_assistant_response(query, openai_api_key):
-    openai.api_key = openai_api_key
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": query}
-        ]
-    )
-    return response['choices'][0]['message']['content']
+        st.session_state.messages.append({"role": "assistant", "content": assistant_response})
 
 if __name__ == '__main__':
     main()
